a2676dd31dc191c7c3d4e0012447a5a06190f45f
craigmcginley <craigmcginley@gmail.com>
2014-07-08 11:47:47 -0400

    user sign up and user model tests written and passing

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -4,21 +4,21 @@ feature "user signs up" do
 
   scenario "with valid information" do
     visit new_user_registration_path
-
-    fill_in "First Name", with: "Craig"
-    fill_in "Last Name", with: "McGinley"
+    save_and_open_page
+    fill_in "user_first_name", with: "Craig"
+    fill_in "user_last_name", with: "McGinley"
     fill_in "Email", with: "craig@example.com"
     fill_in "Password", with: "password123"
     fill_in "Password confirmation", with: "password123"
-    click_on "Sign up"
+    click_on "Sign Up"
 
-    expect(page).to have_content "Welcome to the Fami.ly"
+    expect(page).to have_content "Welcome to the Fami.ly!"
   end
 
   scenario "without required information" do
     visit new_user_registration_path
 
-    click_on "Sign up"
+    click_on "Sign Up"
 
     expect(page).to have_content "Email can't be blank"
     expect(page).to have_content "Password can't be blank"
